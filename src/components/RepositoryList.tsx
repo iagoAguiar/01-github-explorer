dfebba3f97f8d30e95d8f13a3c5265cc0f89a10e
iagoAguiar <iagoaguiar202@gmail.com>
2021-11-06 13:28:09 -0300

    alterado projeto para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,16 +1,20 @@
-import { useState, useEffect} from "react"
 import  React from "react"
+import { useState, useEffect} from "react"
 import { RepositoryItem } from "./RepositoryItem"
 
 import "../styles/repositories.scss"
 
-const repositoryName= 'unform'
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
 
+}
 
 export function RepositoryList(){
 
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() =>{
         fetch('https://api.github.com/users/iagoAguiar/repos')
